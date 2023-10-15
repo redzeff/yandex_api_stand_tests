e1a2dd65fe5520ab6138e9f438ff8a3cdbb94d71
redzeff <minca556@gmail.com>
2023-10-15 23:11:12 +0700

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://877fb9fe-f5b2-4b06-8bb6-45b767c922bd.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+CREATE_USER_PATH = "/api/v1/users/"
